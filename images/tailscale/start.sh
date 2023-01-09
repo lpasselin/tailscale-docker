2a7ef96e17b436f2b96fec615c1b2c728ac47ca4
Ian Neubert <ian@ianneubert.com>
2023-01-09 06:36:56 -0800

    Allow user to set additional Tailscaled options

--- images/tailscale/start.sh
+++ images/tailscale/start.sh
@@ -2,7 +2,7 @@
 trap 'kill -TERM $PID' TERM INT
 echo "Starting Tailscale daemon"
 # -state=mem: will logout and remove ephemeral node from network immediately after ending.
-tailscaled --tun=userspace-networking --state=${TAILSCALE_STATE_ARG} &
+tailscaled --tun=userspace-networking --state=${TAILSCALE_STATE_ARG} ${TAILSCALE_OPT} &
 PID=$!
 until tailscale up --authkey="${TAILSCALE_AUTH_KEY}" --hostname="${TAILSCALE_HOSTNAME}"; do
     sleep 0.1
