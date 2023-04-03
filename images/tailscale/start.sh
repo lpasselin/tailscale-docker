6cb23f6db01ca2957fc318b380bff758a842a172
Sebastian <sebastian@ipe.email>
2023-04-03 14:28:43 +0200

    Added Headscale support by adding --login-server

--- images/tailscale/start.sh
+++ images/tailscale/start.sh
@@ -1,10 +1,18 @@
-#!/bin/ash
+#!/bin/bash
+args="--authkey=${TAILSCALE_AUTH_KEY} --hostname=${TAILSCALE_HOSTNAME}"
+
+if [ -n "$TAILSCALE_LOGIN_SERVER" ]; then
+  # Add the flag to the arguments
+  args="${args} --login-server=${TAILSCALE_LOGIN_SERVER}"
+fi
+
 trap 'kill -TERM $PID' TERM INT
 echo "Starting Tailscale daemon"
+
 # -state=mem: will logout and remove ephemeral node from network immediately after ending.
 tailscaled --tun=userspace-networking --state=${TAILSCALE_STATE_ARG} &
 PID=$!
-until tailscale up --authkey="${TAILSCALE_AUTH_KEY}" --hostname="${TAILSCALE_HOSTNAME}"; do
+until tailscale up $args; do
     sleep 0.1
 done
 tailscale status
